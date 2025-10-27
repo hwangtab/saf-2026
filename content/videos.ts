c6275185c78a774ab0ecbc394d511eb69f4d673f
hwangtab <hwangtab@github.com>
2025-10-27 17:37:38 +0900

    콘텐츠: 아카이브 영상 링크 추가

--- content/videos.ts
+++ content/videos.ts
@@ -13,4 +13,52 @@ export const videos: Video[] = [
     description: '씨앗:페 2023 현장을 담은 영상 아카이브입니다.',
     youtubeId: '-HicY7-natU',
   },
+  {
+    id: '2',
+    title: '씨앗:페 아카이브 영상 1',
+    description: '제공된 유튜브 링크를 통해 확인할 수 있는 씨앗:페 현장 영상입니다.',
+    youtubeId: 'ZFnswRrM01M',
+  },
+  {
+    id: '3',
+    title: '씨앗:페 아카이브 영상 2',
+    description: '제공된 유튜브 링크를 통해 확인할 수 있는 씨앗:페 현장 영상입니다.',
+    youtubeId: 'Tp8UWQudv50',
+  },
+  {
+    id: '4',
+    title: '씨앗:페 아카이브 영상 3',
+    description: '제공된 유튜브 링크를 통해 확인할 수 있는 씨앗:페 현장 영상입니다.',
+    youtubeId: '65QiH2mBHcc',
+  },
+  {
+    id: '5',
+    title: '씨앗:페 아카이브 영상 4',
+    description: '제공된 유튜브 링크를 통해 확인할 수 있는 씨앗:페 현장 영상입니다.',
+    youtubeId: 'zCvlguR9jMc',
+  },
+  {
+    id: '6',
+    title: '씨앗:페 아카이브 영상 5',
+    description: '제공된 유튜브 링크를 통해 확인할 수 있는 씨앗:페 현장 영상입니다.',
+    youtubeId: 'tjmScl936q4',
+  },
+  {
+    id: '7',
+    title: '씨앗:페 아카이브 영상 6',
+    description: '제공된 유튜브 링크를 통해 확인할 수 있는 씨앗:페 현장 영상입니다.',
+    youtubeId: 'd-LeLZcQ4RA',
+  },
+  {
+    id: '8',
+    title: '씨앗:페 아카이브 영상 7',
+    description: '제공된 유튜브 링크를 통해 확인할 수 있는 씨앗:페 현장 영상입니다.',
+    youtubeId: 'XL8WAd_WUw0',
+  },
+  {
+    id: '9',
+    title: '씨앗:페 아카이브 영상 8',
+    description: '제공된 유튜브 링크를 통해 확인할 수 있는 씨앗:페 현장 영상입니다.',
+    youtubeId: 'KK3POxK7U6w',
+  },
 ];
