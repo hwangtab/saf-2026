fc51e67d9a37c22e082b09188a64f026f2b80f7d
hwangtab <hwangtab@github.com>
2026-01-08 14:27:08 +0900

    Refactor: Change Hero transition to Wedge (Triangle) style

--- app/page.tsx
+++ app/page.tsx
@@ -54,15 +54,15 @@ export default function Home() {
       {/* Hero Section */}
       <section className="relative min-h-screen flex items-center justify-center py-12 md:py-20">
         <BackgroundSlider />
-        {/* Wave Transition */}
+        {/* Wedge Transition */}
         <div className="absolute bottom-0 left-0 w-full leading-none z-10">
           <svg
             className="w-full h-12 md:h-24 text-canvas-soft fill-current"
-            viewBox="0 0 1440 320"
+            viewBox="0 0 1440 100"
             preserveAspectRatio="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M0,96L48,112C96,128,192,160,288,160C384,160,480,128,576,112C672,96,768,96,864,112C960,128,1056,160,1152,160C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z" />
+            <polygon points="0,100 1440,100 720,0" />
           </svg>
         </div>
         <div className="relative z-10 container-max text-center">
