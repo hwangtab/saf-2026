8471b48a71cb84334cd6858171fca4498428de60
hwangtab <hwangtab@github.com>
2026-02-17 16:35:01 +0900

    style: fix exhibitor onboarding page bottom padding

--- app/exhibitor/onboarding/page.tsx
+++ app/exhibitor/onboarding/page.tsx
@@ -37,7 +37,7 @@ export default async function ExhibitorOnboardingPage() {
     .maybeSingle();
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center pt-12 pb-16 lg:pb-24 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-2xl">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           SAF 2026
